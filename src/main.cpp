4b8285cbc2046f0931240c356a6ab0a21e054720
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2021-12-18 13:47:15 -0500

    First E-Ink display test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,75 @@
-#include <Arduino.h>
+#include <ArduinoJson.h>              // https://github.com/bblanchon/ArduinoJson needs version v6 or above
+#include <WiFi.h>                     // Built-in
+#include "time.h"                     // Built-in
+#include <SPI.h>                      // Built-in 
+#define  ENABLE_GxEPD2_display 1
+#include <GxEPD2_BW.h>
+#include <U8g2_for_Adafruit_GFX.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define SCREEN_WIDTH  800             // Set for landscape mode
+#define SCREEN_HEIGHT 480
+
+enum alignment {LEFT, RIGHT, CENTER};
+
+// Connections for e.g. LOLIN D32
+static const uint8_t EPD_BUSY = 4;  // to EPD BUSY
+static const uint8_t EPD_CS   = 5;  // to EPD CS
+static const uint8_t EPD_RST  = 22; // to EPD RST
+static const uint8_t EPD_DC   = 21; // to EPD DC
+static const uint8_t EPD_SCK  = 18; // to EPD CLK
+static const uint8_t EPD_MISO = 19; // Master-In Slave-Out not used, as no data from display
+static const uint8_t EPD_MOSI = 23; // to EPD DIN
+
+// Connections for e.g. Waveshare ESP32 e-Paper Driver Board
+//static const uint8_t EPD_BUSY = 25;
+//static const uint8_t EPD_CS   = 15;
+//static const uint8_t EPD_RST  = 26; 
+//static const uint8_t EPD_DC   = 27; 
+//static const uint8_t EPD_SCK  = 13;
+//static const uint8_t EPD_MISO = 12; // Master-In Slave-Out not used, as no data from display
+//static const uint8_t EPD_MOSI = 14;
+
+GxEPD2_BW<GxEPD2_750_T7, GxEPD2_750_T7::HEIGHT> display(GxEPD2_750_T7(/*CS=*/ EPD_CS, /*DC=*/ EPD_DC, /*RST=*/ EPD_RST, /*BUSY=*/ EPD_BUSY));   // B/W display
+
+U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;  // Select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
+// Using fonts:
+// u8g2_font_helvB08_tf
+// u8g2_font_helvB10_tf
+// u8g2_font_helvB12_tf
+// u8g2_font_helvB14_tf
+// u8g2_font_helvB18_tf
+// u8g2_font_helvB24_tf
+
+const char HelloWorld[] = "boobs.";
+
+void helloWorld()
+{
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center the bounding box by transposition of the origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(HelloWorld);
+  }
+  while (display.nextPage());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//#########################################################################################
+void setup() {
+  display.init();
+  helloWorld();
+  display.hibernate();
+}
+//#########################################################################################
+void loop() { // this will never run!
 }
